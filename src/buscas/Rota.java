a2a391956554b955748672cf67c018b56df875fc
Diego Cunha <diego2912@gmail.com>
2015-04-07 10:56:11 -0300

    Update Rota.java

--- src/buscas/Rota.java
+++ src/buscas/Rota.java
@@ -2,13 +2,13 @@ package buscas;
 
 public class Rota {
 	public int id;
-	public String cidade1;
-	public String cidade2;
+	public Cidade cidade1;
+	public Cidade cidade2;
 	public float distancia;
 	public int qualidade;
 	
 	
-    public Rota(int id, String cidade1, String cidade2, float distancia, int qualidade){
+    public Rota(int id, Cidade cidade1, Cidade cidade2, float distancia, int qualidade){
     	this.id = id;
     	this.cidade1 = cidade1;
     	this.cidade2 = cidade2;
